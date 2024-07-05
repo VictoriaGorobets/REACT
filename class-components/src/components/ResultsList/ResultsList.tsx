4582789aa77a3adfb9b155735a81f76211c63535
Victoria Gorobets <vickyhome1996@gmail.com>
2024-07-05 15:48:41 +0300

    feat: implement UI for search bar

--- class-components/src/components/ResultsList/ResultsList.tsx
+++ class-components/src/components/ResultsList/ResultsList.tsx
@@ -1,24 +1,44 @@
-import React, { Component } from 'react';
+import { Component } from 'react';
 import styles from './ResultsList.module.css';
 
-export default class ResultsList extends Component {
-	render() {
-		const results = [
-			{ name: 'Item 1', description: 'Description for item 1' },
-			{ name: 'Item 2', description: 'Description for item 2' },
-			{ name: 'Item 3', description: 'Description for item 3' },
-		];
-
-		return (
-			<div className={styles.resultsList}>
-				{results.map((result, index) => (
-					<div key={index} className={styles.resultItem}>
-						<h3>{result.name}</h3>
-						<p>{result.description}</p>
-					</div>
-				))}
-			</div>
-		);
-	}
+interface Result {
+  id: number;
+  name: string;
+  description: string;
 }
 
+interface ResultsListProps {}
+
+interface ResultsListState {
+  results: Result[];
+}
+
+class ResultsList extends Component<ResultsListProps, ResultsListState> {
+  constructor(props: ResultsListProps) {
+    super(props);
+    this.state = {
+      results: [
+        { id: 1, name: 'Item 1', description: 'Description for item 1' },
+        { id: 2, name: 'Item 2', description: 'Description for item 2' },
+        { id: 3, name: 'Item 3', description: 'Description for item 3' },
+      ],
+    };
+  }
+
+  render() {
+    const { results } = this.state;
+
+    return (
+        <div className={styles.resultsList}>
+          {results.map((result) => (
+              <div key={result.id} className={styles.resultItem}>
+                <h3>{result.name}</h3>
+                <p>{result.description}</p>
+              </div>
+          ))}
+        </div>
+    );
+  }
+}
+
+export default ResultsList;
---
+++ class-components/src/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import styles from './SearchBar.module.css';
+
+interface SearchBarProps {}
+
+interface SearchBarState {
+	searchTerm: string;
+}
+
+class SearchBar extends Component<SearchBarProps, SearchBarState> {
+	constructor(props: SearchBarProps) {
+		super(props);
+		this.state = {
+			searchTerm: '',
+		};
+	}
+
+	handleSearch = () => {
+		const { searchTerm } = this.state;
+		// Handle search logic will be here
+		alert(`Searching for: ${searchTerm}`);
+	};
+
+	handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		this.setState({ searchTerm: e.target.value });
+	};
+
+	render() {
+		const { searchTerm } = this.state;
+
+		return (
+			<div className={styles.searchBar}>
+				<input
+					type="text"
+					placeholder="Search..."
+					value={searchTerm}
+					onChange={this.handleInputChange}
+				/>
+				<button type="button" onClick={this.handleSearch}>
+					Search
+				</button>
+			</div>
+		);
+	}
+}
+
+export default SearchBar;
